f6ba0058bf4c33970061648f2506dcc5f0d9bde5
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-10-12 23:57:05 +0530

    files addede

---
+++ JS/script.js
@@ -0,0 +1,115 @@
+const homedisplayimages = document.querySelector(".displaysection");
+const navactionlist = document.querySelectorAll(".navactionlist li");
+const tital = document.title;
+const personPopularSection = document.querySelector(".personPopularSection");
+
+
+
+
+
+
+if (tital == "My Movies - Your go-to destination for a vast collection of movies and series.") {
+    // console.log("Home");
+    navactionlist[0].style.opacity = "1";
+} else if (tital == "Movies - Your go-to destination for a vast collection of movies and series.") {
+    // console.log("movies");
+    navactionlist[1].style.opacity = "1";
+}
+else {
+    navactionlist[2].style.opacity = "1";
+    // console.log("tv");
+}
+
+homeDisplayImage();
+
+async function homeDisplayImage() {
+    let pageNumber = 1;
+
+    try {
+        let index = 0;
+
+        const responce = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=20f9ed2296f2b4b0100817e7a4262e8f&page=${pageNumber}`);
+        const data = await responce.json()
+        // console.log(data.results);
+        // console.log(data.results[0].known_for[0].backdrop_path);
+
+        const div = document.createElement("div");
+        div.classList.add("images");
+        div.innerHTML =
+            `<img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">
+        <img src="" alt="Images">`;
+
+        homedisplayimages.append(div);
+
+        const homedisplayimg = homedisplayimages.querySelectorAll(".images img")
+
+        if (index <= homedisplayimg.length) {
+            for (let index = 0; index < homedisplayimg.length; index++) {
+                homedisplayimg[index].src = `https://image.tmdb.org/t/p/original${data.results[index].known_for[0].backdrop_path}`;
+            }
+        } else {
+            console.log("outtttt", index)
+        }
+
+
+        // ----------------personPopularSection---------------------
+
+        // console.log(data.results[0].known_for[0].poster_path);
+
+
+
+        for (let index = 0; index < data.results.length; index++) {
+
+            const imagepath = data.results[index].known_for[0].poster_path;
+            const rating = data.results[index].known_for[0].vote_average;
+            const votes = data.results[index].known_for[0].vote_count;
+            const titleis = data.results[index].known_for[0].title
+
+            const popularcards = document.createElement("div")
+            popularcards.classList.add("card");
+            popularcards.innerHTML =
+                `<div class="image">
+                <img src="https://image.tmdb.org/t/p/original${imagepath}" alt="poster-image">
+                </div>
+        
+                <div class="details">
+                <h2 class="title">${titleis}</h2>
+                <div class="info">
+                    <p>Rating: <span class="rating">${rating}</span></p>
+                    <p>Votes: <span class="votecount">${votes}</span></p>
+                </div>
+                </div>`;
+            personPopularSection.append(popularcards);
+
+        }
+
+        // console.log(data.page);
+        // console.log(data.total_pages);
+        // console.log(pageNumber)
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+}
